e403e3c958be936e0afe297328a490f88e2eed73
Merve Asiler <asiler@ceng.metu.edu.tr>
2022-05-05 00:15:40 +0300

    ..

--- Puzzle.cpp
+++ Puzzle.cpp
@@ -1,1 +1,18 @@
 #include "Puzzle.h"
+
+Puzzle::Puzzle() {
+
+}
+
+Puzzle::~Puzzle() {
+
+}
+
+Puzzle Puzzle::crop(int[3], int[3]) const {
+
+}
+
+void Puzzle::patch(Puzzle) {
+
+}
+
--- Puzzle.h
+++ Puzzle.h
@@ -5,4 +5,14 @@
 
 class Puzzle {
 
+	Puzzle* top_left;
+	Puzzle* top_right;
+	Puzzle* bottom_left;
+	Puzzle* bottom_right;
+
+public:
+	Puzzle();
+	~Puzzle();
+	Puzzle crop(int[3], int[3]) const ;
+	void patch(Puzzle);
 };
